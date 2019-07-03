744d14c9446d26b6d8bc7d6c736d42686b23a33d
unknown <you@example.com>
2019-07-03 15:02:08 +0300

    Created

--- server/service/contracts.js
+++ server/service/contracts.js
@@ -2,7 +2,7 @@ const config = require('../config/config')
 const Game = require('../models/Game')
 const Member = require('../models/Member')
 const Web3 = require('web3')
-const gameSetting = require('../config/game-settings.js')()
+const gameSettings = require('../config/game-settings.js')()
 const web3 = new Web3(new Web3.providers.WebsocketProvider(gameSettings.websocketProvider))
 
 

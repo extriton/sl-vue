a1b8d03e20201a53eb52ba9fe5a30c0da841f9c3
unknown <you@example.com>
2019-07-08 20:01:25 +0300

    Created

--- src/components/GamePlay.vue
+++ src/components/GamePlay.vue
@@ -24,7 +24,43 @@
                 <span class="number">{{ timerStr.charAt(7) }}</span>
             </div>
         </div>
+        <!-- Game Info -->
+        <div class="loto-info">
+            <!-- Game Number -->
+            <div class="column">
+                <div class="icon">
+                    <img src="../../public/img/icons/icon_number.png"  alt="Game number" title="Game number" width="50" height="50" />
+                </div>
+                <span>
+                    {{ dict.play_game_num }} <br />
+                    <strong class="text">{{ GameNum }}</strong>
+                </span>
+            </div>
+            <!-- Game Number -->
+            <div class="column">
+                <div class="icon">
+                    <img src="../../public/img/icons/icon_prize.png"  alt="Prize fund" title="Prize fund" width="50" height="50" />
+                </div>
+                <span>
+                    {{ dict.play_prize_fund }} <br />
+                    <strong class="text">{{ Fund }}</strong>
+                </span>
+            </div>
+            <!-- Game Number -->
+            <div class="column">
+                <div class="icon">
+                    <img src="../../public/img/icons/icon_jackpot.png"  alt="Jackpot" title="Jackpot" width="50" height="50" />
+                </div>
+                <span>
+                    {{ dict.play_jackpot }} <br />
+                    <strong class="text">{{ Jackpot }}</strong>
+                </span>
+            </div>
+        </div>
+        
+        <!--
         <pre style="text-align: left;">{{ gameCurrentDetail }}</pre>
+        -->
     </div>
 </template>
 
@@ -70,16 +106,63 @@ export default {
 
             return (days + hours + minutes + seconds)            
         },
-        ...mapGetters(['gameSettings', 'gameCurrent', 'gameCurrentDetail'])
+        GameNum () {
+            if(this.gameCurrentDetail === null) return null
+            else return this.formatNumber(this.gameCurrentDetail.GameNum, 7, 0)
+        },
+        Fund () {
+            if(this.gameCurrentDetail === null) return null
+            else return this.formatNumber(this.gameCurrentDetail.Fund, 1, 4)
+        },
+        Jackpot () {
+            if(this.gameCurrentDetail === null) return null
+            else return this.formatNumber(this.gameCurrentDetail.Jackpot, 1, 4)
+        },
+        ...mapGetters(['gameSettings', 'gameCurrentIndex', 'gameCurrent', 'gameCurrentDetail'])
     },
     methods: {
+        formatNumber (value, int, frac) {
+
+            if(!value) return '-------'
+            
+            const zeroString = '000000000'
+            let _value = '' + value
+            let _int = '' + parseInt(value)
+            let _frac = '' + (value - parseInt(value))
+            _frac = _frac.substr(2)
+
+            // Integer part
+            if(int > _int.length) _int = zeroString.substr(0, int - _int.length) + _int
+            
+            // Fractional part
+            if(frac === 0) return _int
+            if(frac === _frac.length) return _int + '.' + _frac
+
+            if(frac > _frac.length) 
+                _frac += zeroString.substr(0, frac - _frac.length)
+            else
+                _frac = _frac.substr(0, frac)
+
+            return _int + '.' + _frac
+
+        }
+    },
+    watch: {
+        gameCurrentIndex: function(val) { // Перенести в mounted таймер и взять настройки из settings
+            if(val !== null) {
+                this.timerInterval = setInterval(() => {
+                    if(this.timer > 1000) this.timer -= 1000
+                    else this.timer = 0
+                }, 1000)
+            }
+        }
     },
     mounted () {
-        const self = this
-        this.timerInterval = setInterval(function() {
-            if(self.timer > 1000) self.timer -= 1000;
-            else self.timer = 0;
-        }, 1000)
+        console.log('Mount component' + this.gameCurrent.type)
+    },
+    beforeDestroy () {
+        //console.log('Detroy component' + this.gameCurrent.type)
+        if(this.timerInterval !== null) clearInterval(this.timerInterval)
     }
 }
 </script>
@@ -134,6 +217,28 @@ export default {
             .number {
                 background-color: #000;
                 border-radius: 4px;
+            }
+        }
+    }
+    .loto-info {
+        width: 600px; 
+        margin: 20px auto;
+        padding: 20px;
+        background-color: rgba(0, 0, 0, 0.7);
+        box-shadow: 0 0 12px rgba(0, 0, 0, 0.1);
+        border-radius: 10px;
+        color: #FAFAFA;
+        .column {
+            display: inline-block;
+            width: 30%;
+            &:first-child {
+                margin-right: 5%;
+            }
+            &:last-child {
+                margin-left: 5%;
+            }
+            .text {
+                color: #34bbff;
             }
         }
     }

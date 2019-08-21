4a656026d10fa4eeb98b2f9a14897f0f8dd8e6bc
unknown <you@example.com>
2019-08-21 15:45:31 +0300

    Created

--- vue.config.js
+++ vue.config.js
@@ -4,39 +4,9 @@ module.exports = {
         plugins: [
             new webpack.IgnorePlugin(/config\/abi\//),
             new webpack.IgnorePlugin(/config\/server\//),
-            /*
-            new webpack.IgnorePlugin(/config-server.js$/),
-            new webpack.IgnorePlugin(/config-server-mainnet.js$/),
-            new webpack.IgnorePlugin(/config-server-ropsten.js$/),
-            */
             new webpack.IgnorePlugin(/server\//),
             new webpack.IgnorePlugin(/tests\//),
             new webpack.IgnorePlugin(/contracts\//),
-            
-            /*
-            new webpack.IgnorePlugin({
-                resourceRegExp: /^\.\/config-server.js$/,
-                contextRegExp: /config$/
-            }),
-            new webpack.IgnorePlugin({
-                resourceRegExp: /^\.\/config-server-mainnet.js$/,
-                contextRegExp: /config$/
-            }),
-            new webpack.IgnorePlugin({
-                resourceRegExp: /^\.\/config-server-ropsten.js$/,
-                contextRegExp: /config$/
-            })
-            */
         ]
-        /*
-        module: {
-            rules: [
-              {
-                test: /\.js$/,
-                exclude: /config/
-              }
-            ]
-        }
-        */
     }
   }

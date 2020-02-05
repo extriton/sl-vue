13441d757cc7e562b97c41302bd726b9f070b73d
unknown <you@example.com>
2020-02-05 10:55:10 +0300

    bugfix

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // domain: 'https://smart-lotto.org',
-    domain: 'localhost:3000',
+    domain: 'https://smart-lotto.org',
+    // domain: 'localhost:3000',
     ethNetwork: 'mainnet',                      // mainnet, ropsten
     referalRate: 20,                            // (%)
     minimalWithdrawAmount: 0.01,                // (ETH)

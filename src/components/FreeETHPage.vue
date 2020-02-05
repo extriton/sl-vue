c02b79dac2037caf1c133435eda64ca2df110a15
unknown <you@example.com>
2020-02-05 15:21:54 +0300

    bugfix

--- src/components/FreeETHPage.vue
+++ src/components/FreeETHPage.vue
@@ -7,7 +7,7 @@
             <iframe data-aa="1322011" src="//ad.a-ads.com/1322011?size=728x90" scrolling="no" style="width:728px; height:90px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>
         </div>
         <div class="top-banner-small">
-            <iframe data-aa="1322011" src="//ad.a-ads.com/1322011?size=234x60" scrolling="no" style="width:234px; height:60px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>
+            <iframe data-aa="1322104" src="//ad.a-ads.com/1322104?size=234x60" scrolling="no" style="width:234px; height:60px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>
         </div>
         <!-- Luccky numbers table -->
         <table class="lucky-numbers-table">
@@ -76,7 +76,7 @@
             <iframe data-aa="1322054" src="//ad.a-ads.com/1322054?size=728x90" scrolling="no" style="width:728px; height:90px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>
         </div>
         <div class="bottom-banner-small">
-            <iframe data-aa="1322054" src="//ad.a-ads.com/1322054?size=234x60" scrolling="no" style="width:234px; height:60px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>
+            <iframe data-aa="1322105" src="//ad.a-ads.com/1322105?size=234x60" scrolling="no" style="width:234px; height:60px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>
         </div>
         <!--
         <div class="banners-list-left">
